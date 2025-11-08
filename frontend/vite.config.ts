806ddb41d89a1ca857ab1a2c80d6fff8910bec26
Thomas-David Griedel <griedel911@gmail.com>
2025-11-08 18:58:28 +0100

    Update vite.config.ts for GHPages

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // For project pages:
+  base: '/cassini_hackathon/',
+
+  // For user/org site or custom domain, use:
+  // base: '/'
 })
